8923a11eb4fccf9c9ed2eeb026a476ee4024fcc8
황현종 <munidon@naver.com>
2026-04-20 00:43:21 +0900

    d

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAClOV4xinnSx5GyphVmDVItB689Hq9zO0",
+    authDomain: "re-calendar-fcf87.firebaseapp.com",
+    projectId: "re-calendar-fcf87",
+    storageBucket: "re-calendar-fcf87.firebasestorage.app",
+    messagingSenderId: "895510421986",
+    appId: "1:895510421986:web:5cf3fc6dcae680b2a2d9bb"
+};
