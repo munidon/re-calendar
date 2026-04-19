898232cff002c388ed8e3155523fae65c824d625
황현종 <munidon@naver.com>
2026-04-20 00:35:26 +0900

    d

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
+import { firebaseConfig } from './config.js';
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
 import { getFirestore, doc, getDoc, setDoc } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 
 // ===== Firebase 설정 =====
-const firebaseConfig = {
-  apiKey: "AIzaSyAClOV4xinnSx5GyphVmDVItB689Hq9zO0",
-  authDomain: "re-calendar-fcf87.firebaseapp.com",
-  projectId: "re-calendar-fcf87",
-  storageBucket: "re-calendar-fcf87.firebasestorage.app",
-  messagingSenderId: "895510421986",
-  appId: "1:895510421986:web:5cf3fc6dcae680b2a2d9bb"
-};
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
